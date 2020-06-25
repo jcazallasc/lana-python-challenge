e301f6b41e43936e1e1a02147687ef960f26bd20
Javier Cazallas <cazallas@MacBook-Pro-de-Javier.local>
2020-06-25 21:11:51 +0200

    Adapt repositories to work with entities

--- app/checkout_backend/adapters/django/django_offer_repository.py
+++ app/checkout_backend/adapters/django/django_offer_repository.py
@@ -1,10 +1,19 @@
 from typing import List
 
+from checkout_backend.entities.offer_entity import OfferEntity
 from checkout_backend.interfaces.offer_repository import OfferRepository
 from checkout_backend.models import Offer
 
 
 class DjangoOfferRepository(OfferRepository):
 
-    def all(self) -> List[Offer]:
-        return Offer.objects.all()
+    def update_or_create(self, product_code: str, defaults: dict) -> OfferEntity:
+        offer, created = Offer.objects.update_or_create(
+            product__code=product_code,
+            defaults=defaults,
+        )
+
+        return offer.to_entity()
+
+    def all(self) -> List[OfferEntity]:
+        return [offer.to_entity() for offer in Offer.objects.all()]
